f198e42385091d9b2d4adfdeca772400710fe1d8
Pomegranule <elleg1@gmx.com>
2021-05-22 18:10:51 +0100

    All value are read, except select value.
    I managed to get Javascript to read the value of all the answers given by the user in the form. However, the select value is still not read, so I suppose I'm doing that wrong.
    
    But  I suppose I can stop crying now.

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-const userName = document.getElementById("namePlease");
-const userDeal = document.getElementById("yourDeal");
-const userColour = document.getElementById("yourColour");
-const userDate = document.getElementById("date");
-const userFruit = document.getElementById("fruitPicker");
+// const userName = document.getElementById("namePlease");
+// const userDeal = document.getElementById("yourDeal");
+// const userColour = document.getElementById("yourColour");
+// const userDate = document.getElementById("date");
+// const userFruit = document.getElementById("fruitPicker");
 
 // const userInputs = document.getElementsByClassName("userInput");
 
-const userArray = [];
+// const userArray = [];
 
-function yupButton(){
-    userArray;
-    userArray.push(userName, userDeal, userColour, userDate, userFruit);
+// function yupButton(){
+//     userArray;
+//     userArray.push(userName, userDeal, userColour, userDate, userFruit);
 
-}
+// }
 
-const saveFile = {
-    userName: userArray,
-}
-
-function saveButton(saveFile){
+// const saveFile = {
+//     userName: userArray,
+// }
+const yupButton = document.getElementById("yupButton").addEventListener("click", function(){
     // function saveToLocalStorage() {
     //     localStorage.setItem(saveFile.userName, userArray);
     // }
     // saveToLocalStorage();
 
-    function createElement(userName, userDeal, userColour, userDate, userFruit) {
-        const newColDiv = document.createElement("div", {is: "col"});
-        const newColP = document.createElement("p", {is: "contenteditable"});
-        const newColInfo = document.createTextNode("ID: " + userName + ".<br>" + "Their deal is:<br>" + userDeal + "<br>" + "Their favourite colour is: " + userColour + "The fruit that was most appealing to them was: " + userFruit + "<br>" + "Created on: " + userDate);
-        newColDiv.appendChild(newColP);
-        newColP.appendChild(newColInfo);
-        const randoDiv = document.getElementById("randoCol");
-        randoDiv.append(newColDiv);
-    }
-    createElement();
-}
+    const userName = document.getElementById("namePlease").value;
+    const userDeal = document.getElementById("yourDeal").value;
+    const userColour = document.getElementById("yourColour").value;
+    const userDate = document.getElementById("date").value;
+    // const userFruit = document.getElementById("fruitPicker").value;
+    
+    localStorage.setItem("namePlease", userName);
+
+    console.log(userName);
+
+    const newColDiv = document.createElement("div", {is: "col"});
+    const newColP = document.createElement("p", {is: "contenteditable"});
+    const newColInfo = document.createTextNode("IGN: " + userName + ".<br>" + "Their deal is:<br>" + userDeal + "<br>" + " Their favourite colour is: " + userColour + ". The fruit that was most appealing to them was: " +  "<br>" + " Created on: " + userDate);
+    newColDiv.appendChild(newColP);
+    newColP.appendChild(newColInfo);
+    const randoDiv = document.getElementById("randoCol");
+    randoDiv.append(newColDiv);
+});
 
 
