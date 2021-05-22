10d0dbfb14dffb3f778473dd0741ec8f11651801
Pomegranule <elleg1@gmx.com>
2021-05-22 19:10:58 +0100

    Okay. I did it.
    The fruitPicker value was not being read because I was calling the "fruitPicker" selector using it's id. It didn't have an id called "fruitPicker" it has a NAME called "fruitPicker". I deleted it's name and gave it the id of "fruitPicker" and now it works. Yay.

--- script.js
+++ script.js
@@ -17,6 +17,8 @@
 // const saveFile = {
 //     userName: userArray,
 // }
+
+
 const yupButton = document.getElementById("yupButton").addEventListener("click", function(){
     // function saveToLocalStorage() {
     //     localStorage.setItem(saveFile.userName, userArray);
@@ -26,18 +28,15 @@ const yupButton = document.getElementById("yupButton").addEventListener("click",
     const userName = document.getElementById("namePlease").value;
     const userDeal = document.getElementById("yourDeal").value;
     const userColour = document.getElementById("yourColour").value;
-    const userDate = document.getElementById("date").value;
-    // const userFruit = document.getElementById("fruitPicker").value;
+    const userDate = document.querySelector("#date").value;
+    const userFruit = document.querySelector("#fruitPicker").value;
     
-    localStorage.setItem("namePlease", userName);
+    // localStorage.setItem("namePlease", userName);
 
-    console.log(userName);
-
+    
     const newColDiv = document.createElement("div", {is: "col"});
-    const newColP = document.createElement("p", {is: "contenteditable"});
-    const newColInfo = document.createTextNode("IGN: " + userName + ".<br>" + "Their deal is:<br>" + userDeal + "<br>" + " Their favourite colour is: " + userColour + ". The fruit that was most appealing to them was: " +  "<br>" + " Created on: " + userDate);
-    newColDiv.appendChild(newColP);
-    newColP.appendChild(newColInfo);
+    const newColInfo = document.createTextNode("IGN: " + userName + ".<br>" + "Their deal is:<br>" + userDeal + "<br>" + " Their favourite colour is: " + userColour + ". The fruit that was most appealing to them was: " +  "<br>" + userFruit + " Created on: " + userDate);
+    newColDiv.appendChild(newColInfo);
     const randoDiv = document.getElementById("randoCol");
     randoDiv.append(newColDiv);
 });
