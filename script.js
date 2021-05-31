754842814c9834ea1fec6141b5b1abdaa3fc296f
Pomegranule <elleg1@gmx.com>
2021-05-31 18:20:18 +0100

    So, I actually got it to save.
    Inputs are now saved as strings, which is wonderful. HOWEVER. I now have no way of returning them in the form of more cards. I lso have the issue of them not being unique. I don't have anyway of calling them back, so I've obviously misssed a step somewhere. I thought my code was too short.
    
    Go to bed. Your head hurts.

--- script.js
+++ script.js
@@ -1,56 +1,45 @@
-
-
-// const userInputs = document.getElementsByClassName("userInput");
-
-// const userArray = [];
-
-// function yupButton(){
-//     userArray;
-//     userArray.push(userName, userDeal, userColour, userDate, userFruit);
-
-// }
-
-// const saveFile = {
-//     userName: userArray,
-// }
-
-
 const yupButton = document.getElementById("yupButton").addEventListener("click", function(){
-    // function saveToLocalStorage() {
-    //     localStorage.setItem(saveFile.userName, userArray);
-    // }
-    // saveToLocalStorage();    
-    // localStorage.setItem("namePlease", userName);
-
-    const userName = document.getElementById("namePlease").value;
-    const userDeal = document.getElementById("yourDeal").value;
-    const userColour = document.getElementById("yourColour").value;
-    const userDate = document.querySelector("#date").value;
-    const userFruit = document.querySelector("#fruitPicker").value;
+    
+    let peepName = document.getElementById("namePlease").value;
+    let peepDeal = document.getElementById("yourDeal").value;
+    let peepColour = document.getElementById("yourColour").value;
+    let peepDate = document.querySelector("#date").value;
+    let peepFruit = document.querySelector("#fruitPicker").value;
     
     const newColDiv = document.createElement("div", {is: "col"});
     newColDiv.setAttribute("id", "ignDiv");
-    newColDiv.innerHTML = ("<fieldset>IGN: <h5>" + userName + ".</h5>" + "Their deal is:<h5>" + userDeal + "</h5>" + " Their favourite colour is: <h5>" + userColour + ".</h5>The fruit that was most appealing to them was: <h5>" + userFruit + ".</h5> Created on: <h5>" + userDate + "</h5></fieldset>");
+    newColDiv.innerHTML = ("<fieldset>IGN: <h5>" + peepName + ".</h5>" + "Their deal is:<h5>" + peepDeal + "</h5>" + " Their favourite colour is: <h5>" + peepColour + ".</h5>The fruit that was most appealing to them was: <h5>" + peepFruit + ".</h5> Created on: <h5>" + peepDate + "</h5></fieldset>");
     const randoDiv = document.getElementById("randoCol");
     randoDiv.append(newColDiv);
 
-    storeIGN();
+    storePeep();
 });
 
+// const userName = document.getElementById("namePlease").value;
+// const userDeal = document.getElementById("yourDeal").value;
+// const userColour = document.getElementById("yourColour").value;
+// const userDate = document.querySelector("#date").value;
+// const userFruit = document.querySelector("#fruitPicker").value;
 
+function storePeep() {
 
-function storeIGN(userName, userDeal, userColour, userDate, userFruit) {
+    let peepName = document.getElementById("namePlease").value;
+    let peepDeal = document.getElementById("yourDeal").value;
+    let peepColour = document.getElementById("yourColour").value;
+    let peepDate = document.querySelector("#date").value;
+    let peepFruit = document.querySelector("#fruitPicker").value;
+
     localStorage.setItem("ourPeeps", JSON.stringify({
-        "userName": userName,
-        "userDeal": userDeal,
-        "userColour": userColour,
-        "userDate": userDate,
-        "userFruit": userFruit
+        "userName": peepName,
+        "userDeal": peepDeal,
+        "userColour": peepColour,
+        "userDate": peepDate,
+        "userFruit": peepFruit,
     }));
     console.log(localStorage);
 }
 
-const loadPeeps = document.getElementsByTagName("body").addEventListener("load", function(){
+const loadPeeps = document.getElementById("loadOtherPeeps").addEventListener("click", function(){
     let peeps = JSON.parse(localStorage.getItem("ourPeeps"));
 
     console.log(peeps);
