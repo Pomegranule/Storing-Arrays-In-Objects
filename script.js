c59da3bb6c58a1549c167361da48ac82ec862d59
Pomegranule <elleg1@gmx.com>
2021-06-04 17:37:28 +0100

    Everything sucks and I'm terrible at this why
    - Made a new script, but then went back to old one because the functions kept functioning when I hadn't even called them, and I specifically told them to only function when they "hear" a click. Rude.
    
    -Had a little bit of an epiphany ; I now understand why people were taking whole chunks of html and inserting it into the JS - they were storing the entire chunk of HTML and then telling the script to display in it an existing HTML element. Clever bastards.
    
    - Forget about DRY, just declay as much as you need to and sort DRY out later. Damn communists can suck my toe, istg.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let uniquePeepID = "ID_" + Math.random().toString(36).slice(2);
+
 const yupButton = document.getElementById("yupButton").addEventListener("click", function(){
     
     let peepName = document.getElementById("namePlease").value;
@@ -8,19 +10,12 @@ const yupButton = document.getElementById("yupButton").addEventListener("click",
     
     const newColDiv = document.createElement("div", {is: "col"});
     newColDiv.setAttribute("id", "ignDiv");
-    newColDiv.innerHTML = ("<fieldset>IGN: <h5>" + peepName + ".</h5>" + "Their deal is:<h5>" + peepDeal + "</h5>" + " Their favourite colour is: <h5>" + peepColour + ".</h5>The fruit that was most appealing to them was: <h5>" + peepFruit + ".</h5> Created on: <h5>" + peepDate + "</h5></fieldset>");
+    newColDiv.innerHTML = ("<fieldset>IGN: <h5>" + peepName + ".</h5>" + "Their deal is:<h5>" + peepDeal + "</h5>" + " Their favourite colour is: <h5>" + peepColour + ".</h5>The fruit that was most appealing to them was: <h5>" + peepFruit + ".</h5> Created on: <h5>" + peepDate + "</h5>" + uniquePeepID + "</fieldset>");
     const randoDiv = document.getElementById("randoCol");
     randoDiv.append(newColDiv);
 
     storePeep();
 });
-
-// const userName = document.getElementById("namePlease").value;
-// const userDeal = document.getElementById("yourDeal").value;
-// const userColour = document.getElementById("yourColour").value;
-// const userDate = document.querySelector("#date").value;
-// const userFruit = document.querySelector("#fruitPicker").value;
-let uniquePeepID = "ID_" + Math.random().toString(36).slice(2);
 
 function storePeep() {
 
@@ -28,9 +23,8 @@ function storePeep() {
     let peepDeal = document.getElementById("yourDeal").value;
     let peepColour = document.getElementById("yourColour").value;
     let peepDate = document.querySelector("#date").value;
-    let peepFruit = document.querySelector("#fruitPicker").value;
-    
-    
+    let peepFruit = document.querySelector("#fruitPicker").value;    
+
 
     localStorage.setItem(uniquePeepID, JSON.stringify({
         "userName": peepName,
@@ -42,6 +36,42 @@ function storePeep() {
     }));
     console.log(localStorage);
 }
+
+function peepCards(){  
+    // peeps = localStorage.getItem(uniquePeepID);
+    localStorage.getItem("");
+    console.log(localStorage);
+    // peepName = localStorage.getItem("userName");
+    // peepDeal = localStorage.getItem("userDeal");
+    // peepColour = localStorage.getItem("userColour");
+    // peepDate = localStorage.getItem("userDate");
+    // peepFruit = localStorage.getItem("userFruit");
+    // uniquePeepID = localStorage.getItem("ignID");
+
+    // const newColDiv = document.createElement("div", {is: "col"});
+    // newColDiv.setAttribute("id", "ignDiv");
+    // peepCard = newColDiv.innerHTML = ("<fieldset>IGN: <h5>" + peepName + ".</h5>" + "Their deal is:<h5>" + peepDeal + "</h5>" + " Their favourite colour is: <h5>" + peepColour + ".</h5>The fruit that was most appealing to them was: <h5>" + peepFruit + ".</h5> Created on: <h5>" + peepDate + "</h5></fieldset>");
+
+    // for (let peeps=0; peepCard <= peeps.length; peepCard++){
+    //     const randoDiv = document.getElementById("randoCol");
+    //     randoDiv.append(newColDiv);
+    // }
+
+    
+}
+
+const loadPeeps = document.getElementById("loadOtherPeeps").addEventListener("click", function(){
+    // let peeps = JSON.parse(localStorage.getItem({}, uniquePeepID));
+    // //uniquePeepID is not defined. You cannot redefine it here because then you will get a different unique ID. Instead, you should reference a click event listener, that tracks which peep you have selected, and then gives you the uniquePeepID of the selected peep. Wait, that doesn't work for this because this whole function is to make the peeps load. How can you click on a peep that hasn't loaded on the page, yet?
+
+    // console.log(peeps);
+
+    peepCards();
+
+    const newColDiv = document.createElement("div", {is: "col"});
+    newColDiv.setAttribute("id", "ignDiv");
+    peepCard = newColDiv.innerHTML = ("<fieldset>IGN: <h5>" + peepName + ".</h5>" + "Their deal is:<h5>" + peepDeal + "</h5>" + " Their favourite colour is: <h5>" + peepColour + ".</h5>The fruit that was most appealing to them was: <h5>" + peepFruit + ".</h5> Created on: <h5>" + peepDate + "</h5></fieldset>");
+})
 
 // function peepID(){
 //         let uniquePeepID = 0
@@ -56,26 +86,3 @@ function storePeep() {
 //         return uniquePeepID;
 //         // console.log(uniquePeepID);
 //     }
-
-const loadPeeps = document.getElementById("loadOtherPeeps").addEventListener("click", function(){
-    let peeps = JSON.parse(localStorage.getItem({}, uniquePeepID));
-    //uniquePeepID is not defined. You cannot redefine it here because then you will get a different unique ID. Instead, you should reference a click event listener, that tracks which peep you have selected, and then gives you the uniquePeepID of the selected peep. Wait, that doesn't work for this because this whole function is to make the peeps load. How can you click on a peep that hasn't loaded on the page, yet?
-
-    console.log(peeps);
-
-    peepCards();
-})
-
-function peepCards(){  
-    peeps = localStorage.getItem({}, uniquePeepID);
-    
-    for (let peepCards=0; peepCards <= peeps.length; peepCards++){
-        console.log(peeps);
-        peepCards= newColDiv.innerHTML = ("<fieldset>IGN: <h5>" + peepName + ".</h5>" + "Their deal is:<h5>" + peepDeal + "</h5>" + " Their favourite colour is: <h5>" + peepColour + ".</h5>The fruit that was most appealing to them was: <h5>" + peepFruit + ".</h5> Created on: <h5>" + peepDate + "</h5></fieldset>");
-        const randoDiv = document.getElementById("randoCol");
-        randoDiv.append(newColDiv);
-    }
-
-    
-}
-
